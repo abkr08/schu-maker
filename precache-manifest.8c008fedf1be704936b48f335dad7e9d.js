033267b8a535f7f116b8a76a471f789f7dcc7ffb
Abubakar Ibrahim <abubakaribrahim08@yahoo.com>
2019-03-05 21:43:54 +0300

    Updates

---
+++ precache-manifest.8c008fedf1be704936b48f335dad7e9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2678d0fb91359764e1eb",
+    "url": "/shoemaker/static/js/runtime~main.2678d0fb.js"
+  },
+  {
+    "revision": "4351d46be91c2b7e5a1a",
+    "url": "/shoemaker/static/js/main.4351d46b.chunk.js"
+  },
+  {
+    "revision": "f3d8506020f4365a9ba1",
+    "url": "/shoemaker/static/js/2.f3d85060.chunk.js"
+  },
+  {
+    "revision": "4351d46be91c2b7e5a1a",
+    "url": "/shoemaker/static/css/main.bb16f828.chunk.css"
+  },
+  {
+    "revision": "210a69935efb832273b13181febb2b33",
+    "url": "/shoemaker/index.html"
+  }
+];
